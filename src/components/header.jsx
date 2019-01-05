79f9aac694922b94ad02209d1f85c5de2c41f2d0
JohnIpsen <ipsen247@gmail.com>
2019-01-04 21:24:29 -0800

    think im satisfied with this layout

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,20 +1,13 @@
 import React, { Component } from "react";
-import logo from "../images/ICCLogo.jpg";
+import logo from "../images/ICCLogoTransparent.png";
 import "../styles/header.css";
-import { Image } from "semantic-ui-react";
 
 class Header extends Component {
   render() {
     return (
       <div className="header">
-        <Image
-          id="ICCLogo"
-          src={logo}
-          as="a"
-          size="medium"
-          href="http://google.com"
-          target="_blank"
-        />
+        <div id="header-bg-color" />
+        <img id="ICCLogo" src={logo} as="a" href="/" alt="ICC" />
         {/* <h3>Breeding for Genetics That Matter</h3> */}
       </div>
     );
--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -5,42 +5,45 @@ import { Menu, Segment, Sidebar, Button } from "semantic-ui-react";
 class SideNav extends Component {
   render() {
     return (
-      <Sidebar.Pushable className="side-bar" as={Segment}>
-        <Sidebar
-          className="side-menu"
-          as={Menu}
-          animation="overlay"
-          inverted
-          vertical
-          visible
-          width="thin"
-        >
-          <Menu.Item href="/">
-            <Button className="side-link">Home</Button>
-          </Menu.Item>
-          <Menu.Item href="/about">
-            <Button className="side-link">About</Button>
-          </Menu.Item>
-          <Menu.Item href="/about">
-            <Button className="side-link">Herd Sires</Button>
-          </Menu.Item>
-          <Menu.Item href="/about">
-            <Button className="side-link">Cowherd</Button>
-          </Menu.Item>
-          <Menu.Item href="/about">
-            <Button className="side-link">For Sale</Button>
-          </Menu.Item>
-          <Menu.Item href="/about">
-            <Button className="side-link">Gallery</Button>
-          </Menu.Item>
-          <Menu.Item href="/about">
-            <Button className="side-link">Links</Button>
-          </Menu.Item>
-          <Menu.Item href="/about">
-            <Button className="side-link">Contact</Button>
-          </Menu.Item>
-        </Sidebar>
-      </Sidebar.Pushable>
+      <div>
+        <div className="side-bar" id="side-bg-color" />
+        <Sidebar.Pushable className="side-bar" as={Segment}>
+          <Sidebar
+            className="side-menu"
+            as={Menu}
+            animation="overlay"
+            inverted
+            vertical
+            visible
+            width="thin"
+          >
+            <Menu.Item href="/">
+              <Button className="side-link">Home</Button>
+            </Menu.Item>
+            <Menu.Item href="/about">
+              <Button className="side-link">About</Button>
+            </Menu.Item>
+            <Menu.Item href="/about">
+              <Button className="side-link">Herd Sires</Button>
+            </Menu.Item>
+            <Menu.Item href="/about">
+              <Button className="side-link">Cowherd</Button>
+            </Menu.Item>
+            <Menu.Item href="/about">
+              <Button className="side-link">For Sale</Button>
+            </Menu.Item>
+            <Menu.Item href="/about">
+              <Button className="side-link">Gallery</Button>
+            </Menu.Item>
+            <Menu.Item href="/about">
+              <Button className="side-link">Links</Button>
+            </Menu.Item>
+            <Menu.Item href="/about">
+              <Button className="side-link">Contact</Button>
+            </Menu.Item>
+          </Sidebar>
+        </Sidebar.Pushable>
+      </div>
     );
   }
 }
