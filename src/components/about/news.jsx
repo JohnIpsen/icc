feb92ac26fd39bbd2f15c2eac3ea0eb65f1ea52b
JohnIpsen <ipsen247@gmail.com>
2019-01-06 13:04:48 -0800

    formatted news section.. still needs work

--- src/components/about/news.jsx
+++ src/components/about/news.jsx
@@ -1,23 +1,51 @@
 import React, { Component } from "react";
 import { Item } from "semantic-ui-react";
 import pap from "../../images/news/pap.jpg";
+import salea from "../../images/news/salea.jpg";
 
 class News extends Component {
   render() {
     return (
       <div>
-        <Item.Group>
+        <Item.Group className="news-block">
           <Item>
-            <Item.Image size="tiny" src={pap} />
+            <Item.Image className="news-img" size="tiny" src={pap} />
 
             <Item.Content>
-              <Item.Header>2018 PAP Testing</Item.Header>
+              <Item.Header>
+                <h3 className="news-header">2018 PAP Testing</h3>
+              </Item.Header>
               <Item.Meta>
-                PAP testing finished this last October. Yearling weights are in
-                and EPD's updated. Come see the offering anytime. We are pretty
-                excited about this set of Angus and Hereford bulls. Will have a
-                very nice set of bred heifers offered, including one right out
-                of our ET program.
+                {" "}
+                <p className="news-meta">
+                  PAP testing finished this last October. Yearling weights are
+                  in and EPD's updated. Come see the offering anytime. We are
+                  pretty excited about this set of Angus and Hereford bulls.
+                  Will have a very nice set of bred heifers offered, including
+                  one right out of our ET program.
+                </p>
+              </Item.Meta>
+            </Item.Content>
+          </Item>
+        </Item.Group>
+        <Item.Group className="news-block">
+          <Item>
+            <Item.Image className="news-img" size="tiny" src={salea} />
+
+            <Item.Content>
+              <Item.Header>
+                <h3 className="news-header">2018 Bull Sale Concludes</h3>
+              </Item.Header>
+              <Item.Meta>
+                {" "}
+                <p className="news-meta">
+                  Thank you to all who supported us this year. We are grateful
+                  to all bidders and buyers. Averages: $4100.00 on Fall Angus
+                  Bulls, $3300.00 on Fall Hereford Bulls, $2600.00 on Yearling
+                  Angus Bulls. Lot 18 was our high selling bull overall at
+                  $7,900. Thanks again. We are working now to get the pens ready
+                  and wean next years herd sires.
+                </p>
               </Item.Meta>
             </Item.Content>
           </Item>
